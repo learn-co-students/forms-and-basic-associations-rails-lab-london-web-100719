94352d1c959db0bafe813719c4f01889eabe064d
Liko <li_xie1992@hotmail.com>
2019-11-06 09:14:23 +0000

    setup initial data and possible associations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,7 @@
 class Genre < ActiveRecord::Base
   # add associations
+  has_many :songs
+  has_many :artists, through: :songs
+  #maybe
+  has_many :notes, through: :songs
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,6 @@
 class Note < ActiveRecord::Base
   # add associations here
+  belongs_to :song
+  belongs_to :artist, through: :song
+  belongs_to :genre, through: :song
 end
